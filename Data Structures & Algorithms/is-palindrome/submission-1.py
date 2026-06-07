e0c269be1834b4b9c258b5e8a8ce5c3c31670356
gibranmon <115527859+gibranmon@users.noreply.github.com>
2026-06-06 21:56:55 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,20 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        normalized_string = re.sub(r'[^0-9a-zA-Z]', '', s)
+        
+        normalized_string = normalized_string.lower()
+
+        print(normalized_string)
+        
+        first = 0
+        last = len(normalized_string) - 1
+        while first < len(normalized_string) // 2:
+            if normalized_string[first] != normalized_string[last]:
+                return False
+            first += 1
+            last -= 1
+
+        return True
+        
